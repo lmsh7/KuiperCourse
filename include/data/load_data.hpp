51acb7d33eedf899936e944c5413f6e4786a76a8
LMSH7 <lmsh72238855872@gmail.com>
2023-04-14 10:51:04 +0800

    lec3

--- include/data/load_data.hpp
+++ include/data/load_data.hpp
@@ -4,19 +4,21 @@
 
 #ifndef KUIPER_COURSE_INCLUDE_DATA_LOAD_DATA_HPP_
 #define KUIPER_COURSE_INCLUDE_DATA_LOAD_DATA_HPP_
+
 #include <armadillo>
 #include "data/tensor.hpp"
 
 namespace kuiper_infer {
-class CSVDataLoader {
- public:
-  static std::shared_ptr<Tensor<float >> LoadData(const std::string &file_path, char split_char = ',');
+    class CSVDataLoader {
+    public:
+        static std::shared_ptr<Tensor<float >> LoadData(const std::string &file_path, char split_char = ',');
 
-  static std::shared_ptr<Tensor<float >> LoadDataWithHeader(const std::string &file_path,
-                                       std::vector<std::string> &headers, char split_char = ',');
+        static std::shared_ptr<Tensor<float >> LoadDataWithHeader(const std::string &file_path,
+                                                                  std::vector<std::string> &headers,
+                                                                  char split_char = ',');
 
- private:
-  static std::pair<size_t, size_t> GetMatrixSize(std::ifstream &file, char split_char);
-};
+    private:
+        static std::pair<size_t, size_t> GetMatrixSize(std::ifstream &file, char split_char);
+    };
 }
 #endif //KUIPER_COURSE_INCLUDE_DATA_LOAD_DATA_HPP_
