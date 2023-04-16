47d88c46b1f9e7986f1e816a41aed4ba918b956e
LMSH7 <lmsh72238855872@gmail.com>
2023-04-16 12:19:06 +0800

    lec5

--- include/ops/sigmoid_op.hpp
+++ include/ops/sigmoid_op.hpp
@@ -4,13 +4,15 @@
 
 #ifndef KUIPER_COURSE_INCLUDE_OPS_SIGMOID_OP_HPP_
 #define KUIPER_COURSE_INCLUDE_OPS_SIGMOID_OP_HPP_
+
 #include "op.hpp"
 #include "layer/layer.hpp"
-namespace kuiper_infer {
-class SigmoidOperator : public Operator {
- public:
-  explicit SigmoidOperator();
 
-};
+namespace kuiper_infer {
+    class SigmoidOperator : public Operator {
+    public:
+        explicit SigmoidOperator();
+
+    };
 }
 #endif //KUIPER_COURSE_INCLUDE_OPS_SIGMOID_OP_HPP_
--- source/layer/sigmoid_layer.cpp
+++ source/layer/sigmoid_layer.cpp
@@ -9,36 +9,41 @@
 
 namespace kuiper_infer {
 
-SigmoidLayer::SigmoidLayer(const std::shared_ptr<Operator> &op) : Layer("Sigmoid") {
-  SigmoidOperator *sigmoid_op = dynamic_cast<SigmoidOperator *>(op.get());
+    SigmoidLayer::SigmoidLayer(const std::shared_ptr<Operator> &op) : Layer("Sigmoid") {
+        SigmoidOperator *sigmoid_op = dynamic_cast<SigmoidOperator *>(op.get());
 
-  CHECK(sigmoid_op != nullptr) << "Sigmoid operator is empty";
-  this->op_ = std::make_unique<SigmoidOperator>();
-}
+        CHECK(sigmoid_op != nullptr) << "Sigmoid operator is empty";
+        this->op_ = std::make_unique<SigmoidOperator>();
+    }
 
-void SigmoidLayer::Forwards(const std::vector<std::shared_ptr<Tensor<float>>> &inputs,
-                            std::vector<std::shared_ptr<Tensor<float>>> &outputs) {
-  CHECK(this->op_ != nullptr);
-  CHECK(this->op_->op_type_ == OpType::kOperatorSigmoid);
-  CHECK(!inputs.empty());
+    void SigmoidLayer::Forwards(const std::vector<std::shared_ptr<Tensor<float>>> &inputs,
+                                std::vector<std::shared_ptr<Tensor<float>>> &outputs) {
+        CHECK(this->op_ != nullptr);
+        CHECK(this->op_->op_type_ == OpType::kOperatorSigmoid);
+        CHECK(!inputs.empty());
 
-  const uint32_t batch_size = inputs.size();
-  for (uint32_t i = 0; i < batch_size; ++i) {
-    const std::shared_ptr<Tensor<float>> &input_data = inputs.at(i);
-    std::shared_ptr<Tensor<float>> output_data = input_data->Clone();
+        const uint32_t batch_size = inputs.size();
+        for (uint32_t i = 0; i < batch_size; ++i) {
+            const std::shared_ptr<Tensor<float>> &input_data = inputs.at(i);
+            std::shared_ptr<Tensor<float>> output_data = input_data->Clone();
 //补充,y=1/(1+e^{-x})
 // 参考relu_layer来
 // 或者有自己的想法也可以
-    outputs.push_back(output_data);
-  }
-}
-std::shared_ptr<Layer> SigmoidLayer::CreateInstance(const std::shared_ptr<Operator> &op) {
-  CHECK(op != nullptr);
-  CHECK(op->op_type_ == OpType::kOperatorSigmoid);
+            output_data->data().transform([&](float value) {
+                return 1 / (1 + exp(-value));
+            });
+            outputs.push_back(output_data);
+        }
+    }
 
-  std::shared_ptr<Layer> sigmoid_layer = std::make_shared<SigmoidLayer>(op);
-  return sigmoid_layer;
-}
+    std::shared_ptr<Layer> SigmoidLayer::CreateInstance(const std::shared_ptr<Operator> &op) {
+        CHECK(op != nullptr);
+        CHECK(op->op_type_ == OpType::kOperatorSigmoid);
+
+        std::shared_ptr<Layer> sigmoid_layer = std::make_shared<SigmoidLayer>(op);
+        return sigmoid_layer;
+    }
+
 // OpType::kOperatorRelu 自己替换掉,换成sigmoid对应类型
-//LayerRegistererWrapper kReluLayer1(OpType::kOperatorRelu, SigmoidLayer::CreateInstance);
+    LayerRegistererWrapper kSigmoidLayer(OpType::kOperatorSigmoid, SigmoidLayer::CreateInstance);
 }
